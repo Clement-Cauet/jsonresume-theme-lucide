adb0432f50c27cc518573859e34d10ef6d9d22a6
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2025-02-09 18:31:46 +0100

    refactor: update resume structure and layout

--- theme/hbs-helpers/name-split.js
+++ theme/hbs-helpers/name-split.js
@@ -6,9 +6,9 @@ const nameSplit = (name) => {
   if (parts.length < 2) return name;
 
   const firstName = parts.slice(0, -1).join(" ");
-  const lastName = parts.slice(-1).join(" ");
+  const lastName = parts.slice(-1).join(" ").toUpperCase();
   
-  return `${firstName}<br>${lastName}`;
+  return `${firstName} ${lastName}`;
 };
 
 module.exports = { nameSplit };
